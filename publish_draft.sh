2028b6a3080900d249f5fa7c2ff5c5dd6e557059
WataruSuzuki <wataru0406@hotmail.co.jp>
2020-01-24 22:30:00 +0900

    Updated github actions

--- publish_draft.sh
+++ publish_draft.sh
@@ -1,12 +1,7 @@
-# rm -rf build/distributions/
-#
-# ./gradlew build
-# sleep 1
-
 unzip build/distributions/GitHub-Releases-Drafter-Kotlin.zip -d build/distributions/
 mv build/distributions/GitHub-Releases-Drafter-Kotlin.zip ./
 ./build/distributions/GitHub-Releases-Drafter-Kotlin/bin/GitHub-Releases-Drafter-Kotlin \
     --version=$1 \
     --path="WataruSuzuki/GitHub-Releases-Drafter-Kotlin" \
-    --assetname="GitHub-Releases-Drafter-Kotlin.zip" \
+    --publish \
     --token=${PUBLISH_TOKEN}
